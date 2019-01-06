c3e3f4495234388d7f77a7e3750fea2e85389146
honey96 <harshitpithi1967@gmail.com>
2019-01-07 01:04:07 +0530

    score and layout is completed

--- start.py
+++ start.py
@@ -2,16 +2,29 @@ import pygame
 import OpenGL
 import os
 import time
+import random
 pygame.init()
-width=1080
-height=1080
+width=800
+height=600
 black=(0,0,0)
 white=(100,100,100)
 color=(34,139,34)
+red=(255,0,0)
 surface=pygame.display.set_mode((width,height))
 pygame.display.set_caption('GAME')
 timer=pygame.time.Clock()
 image=pygame.image.load('racecar.png')
+
+def score(count):
+	font = pygame.font.SysFont(None, 25)
+	text = font.render("Score: "+str(count), True,black)
+	surface.blit(text,(0,0))
+
+
+def obstacle(obs_x,obs_y,color,obs_w,obs_h):
+	pygame.draw.rect(surface,color,[obs_x,obs_y,obs_w,obs_h])
+
+
 
 def car(x,y):
 	surface.blit(image,(x,y))
@@ -34,34 +47,55 @@ def end_game():
 
 def start():
 	flag=False	
-	x=(width*0.50)    #1080*0.45
-	y=(height*0.45)
+	x=(width*0.45)    #1080*0.45
+	y=(height*0.8)
 	del_x=0
-	del_y=0
+	obs_x=random.randrange(0,width)
+	obs_y = -600
+	obs_speed = 4
+	obs_w = 100
+	obs_h = 100
+	score_count=0	
 	while not flag:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				flag=True
 			if event.type == pygame.KEYDOWN:
 				if event.key ==pygame.K_LEFT:
-					del_x=-6
+					del_x=-5
 				elif event.key == pygame.K_RIGHT:
-					del_x=6
-				elif event.key ==pygame.K_UP:
-					del_y=-6
-				elif event.key ==pygame.K_DOWN:
-					del_y=6
+					del_x=5
+				
 			if event.type == pygame.KEYUP:
-				if event.key ==pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key ==pygame.K_UP or event.key ==pygame.K_DOWN :
+				if event.key ==pygame.K_LEFT or event.key == pygame.K_RIGHT:
 					del_x=0
-					del_y=0
+					
 
 		x=x+del_x
-		y=y+del_y
 		surface.fill(color)
+		obstacle(obs_x,obs_y,red,obs_w,obs_h)
+		obs_y=obs_y+obs_speed
+
 		car(x,y)
-		if x<0 or y<0 or x > width-75 or y > height-450:
+		score(score_count)
+
+
+		if x<0 or  x > width-73:
 			end_game()
+
+		if obs_y > height:
+			obs_y = 0 - obs_h
+			obs_x =random.randrange(0,width)
+			score_count+=1
+			obs_speed+=1
+			obs_w+= (score_count*1.2)
+
+
+		if y < obs_y+obs_h:
+			print('crossover')
+			if x > obs_x and x < obs_x + obs_w or x+ 73 >obs_x and x+ 73< obs_x + obs_w:
+				print('x crossover')
+				end_game()
 
 		
 		pygame.display.update()
